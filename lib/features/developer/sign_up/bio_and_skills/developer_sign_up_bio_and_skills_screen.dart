a98c34d361aee155d9984a26bac93cad21d7c029
Alghitany <ahmedalghitany12@gmail.com>
2025-03-30 09:48:44 +0200

    -added description regex -added developer_sign_up_optional_data_screen.dart route -modified app text field -modified name conventions -added bio and skills UI

---
+++ lib/features/developer/sign_up/bio_and_skills/developer_sign_up_bio_and_skills_screen.dart
@@ -0,0 +1,39 @@
+import 'package:carrerk/core/helpers/extensions.dart';
+import 'package:carrerk/core/helpers/spacing.dart';
+import 'package:carrerk/core/theming/styles.dart';
+import 'package:carrerk/core/widgets/app_back_icon.dart';
+import 'package:carrerk/core/widgets/app_text_button.dart';
+import 'package:carrerk/features/developer/sign_up/bio_and_skills/widgets/bio_and_skills_form.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+import '../../../../core/routing/routes.dart';
+
+class DeveloperSignUpBioAndSkillsScreen extends StatelessWidget {
+  const DeveloperSignUpBioAndSkillsScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+          child: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 16.0.w, vertical: 16.h),
+        child: Column(
+          children: [
+            const AppBackIcon(),
+            verticalSpace(32),
+            const BioAndSkillsForm(),
+            verticalSpace(48),
+            AppTextButton(
+              buttonText: 'Continue',
+              textStyle: AppTextStyles.font14WhitePoppinsMedium,
+              onPressed: () {
+                //TODO: Check the validation of form fields and navigate
+                context.pushNamed(Routes.developerSignUpOptionalDataScreen);
+              },),
+          ],
+        ),
+      )),
+    );
+  }
+}
