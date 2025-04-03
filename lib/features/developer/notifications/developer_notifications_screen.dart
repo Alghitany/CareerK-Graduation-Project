fa0a2f781c6c4ad35ae9f98eaa9d2b66279b16a1
Alghitany <ahmedalghitany12@gmail.com>
2025-04-03 19:43:45 +0200

    -modified app search width

--- lib/features/developer/notifications/developer_notifications_screen.dart
+++ lib/features/developer/notifications/developer_notifications_screen.dart
@@ -1,10 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class DeveloperNotificationsScreen extends StatelessWidget {
   const DeveloperNotificationsScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: SafeArea(
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 16.0.w, vertical: 16.h),
+          child: Column(
+            children: [
+
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
