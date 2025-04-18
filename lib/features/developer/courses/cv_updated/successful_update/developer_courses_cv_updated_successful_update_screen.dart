9fdda91e64da8d6ca675030e6d61bc76afb68384
Alghitany <ahmedalghitany12@gmail.com>
2025-04-18 22:00:26 +0200

    -added developer_courses_cv_updated_successful_update_screen.dart route -added cv -added update cv UI

---
+++ lib/features/developer/courses/cv_updated/successful_update/developer_courses_cv_updated_successful_update_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class DeveloperCoursesCvUpdatedSuccessfulUpdateScreen extends StatelessWidget {
+  const DeveloperCoursesCvUpdatedSuccessfulUpdateScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
