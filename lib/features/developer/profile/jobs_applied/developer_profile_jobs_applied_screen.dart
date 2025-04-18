1ee0e314e10df37e7da2c97d3cf36582e04b5008
Alghitany <ahmedalghitany12@gmail.com>
2025-04-18 22:37:14 +0200

    -added jobs applied UI

--- lib/features/developer/profile/jobs_applied/developer_profile_jobs_applied_screen.dart
+++ lib/features/developer/profile/jobs_applied/developer_profile_jobs_applied_screen.dart
@@ -1,10 +1,35 @@
+import 'package:carrerk/core/helpers/spacing.dart';
+import 'package:carrerk/core/theming/styles.dart';
+import 'package:carrerk/core/widgets/app_back_icon.dart';
+import 'package:carrerk/features/developer/profile/jobs_applied/widgets/applied_jobs_list.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class DeveloperProfileJobsAppliedScreen extends StatelessWidget {
   const DeveloperProfileJobsAppliedScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: EdgeInsets.fromLTRB(16.w,16.h,16.w,0.h),
+            child: Column(
+              children: [
+                const AppBackIcon(),
+                Text(
+                  'Job Applied',
+                  style: AppTextStyles.font24SecondaryWildBlueYonderPoppinsSemiBold,
+                ),
+                verticalSpace(32),
+                //TODO: Refactoring this container should be done
+                const AppliedJobsList(),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
