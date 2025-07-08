6d4dc22d3a94c789040a48f61c436f36152074a0
Alghitany <ahmedalghitany12@gmail.com>
2025-07-09 01:17:35 +0300

    refactor naming

---
+++ lib/features/company/ui/profile/data/models/company_profile_info_models/company_profile_info_response_body.g.dart
@@ -0,0 +1,35 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'company_profile_info_response_body.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+CompanyProfileInfoResponseBody _$CompanyProfileInfoResponseBodyFromJson(
+        Map<String, dynamic> json) =>
+    CompanyProfileInfoResponseBody(
+      id: json['id'] as String,
+      companyName: json['company_name'] as String,
+      country: json['country'] as String,
+      city: json['city'] as String,
+      address: json['address'] as String,
+      phoneNumber: json['phone_number'] as String,
+      email: json['email'] as String,
+      profilePicture: json['profile_picture'] as String,
+      briefDescription: json['brief_description'] as String,
+    );
+
+Map<String, dynamic> _$CompanyProfileInfoResponseBodyToJson(
+        CompanyProfileInfoResponseBody instance) =>
+    <String, dynamic>{
+      'id': instance.id,
+      'company_name': instance.companyName,
+      'country': instance.country,
+      'city': instance.city,
+      'address': instance.address,
+      'phone_number': instance.phoneNumber,
+      'email': instance.email,
+      'profile_picture': instance.profilePicture,
+      'brief_description': instance.briefDescription,
+    };
